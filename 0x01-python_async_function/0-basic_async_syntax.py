fb0c45794ebeefebf5d03bf1008fa85b78c055e7
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-15 04:42:53 +0100

    rectify pycodestyle error in  0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""This contains an asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10) named wait_random that waits for a random delay between 0 and max_delay (included and float value) seconds and eventually returns it.
+"""This contains an asynchronous coroutine that takes in an integer
+argument (max_delay, with a default value of 10) named wait_random
+that waits for a random delay between 0 and max_delay (included
+and float value) seconds and eventually returns it.
 
 It uses the random module.
 """
@@ -11,6 +14,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     # waits for  a random delay
-    delay = random.random() *  max_delay;
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
-    return delay;
+    return delay
