4f179929f77aebe070277eaff87cb23f08efcee4
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-15 04:55:27 +0100

    fix pycodestyle in 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 """This imports wait_random from the previous 0-basic_async_syntax.py file
 and writes an async routine called wait_n that takes in 2 int arguments
-(in this order): n and max_delay. It will spawn wait_random n times with the specified max_delay.
+(in this order): n and max_delay. It will spawn wait_random n times
+with the specified max_delay.
 wait_n returns the list of all the delays (float values).
 The list of the delays are in ascending order without the use of
 sort() because of concurrency.
