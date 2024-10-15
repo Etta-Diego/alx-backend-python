ee2d0c542f7ef0a383c772a52a37992ee9f13026
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-15 23:26:31 +0100

    add 1-async_comprehension.py 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Import async_generator from 0-async_generator.py file
+async_comprehension: This coroutine will collect 10 random numbers
+using an async comprehensing over async_generator,
+then returns the 10 random numbers.
+It takes no arguments."""
+
+
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Use an async comprehension to collect all 10 numbers into a list."""
+    return [rand_num async for rand_num in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Imports async_comprehension from the 1-async_comprehension.pyfile."""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+"""Measure_runtime: a coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
+It measures the total runtime and return it."""
+
+
+async def measure_runtime():
+    """Starts the timer"""
+    start_time = time.perf_counter()
+    """Run async_comprehension four times in parallel"""
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension())
+    """Stops the timer"""
+    end_time = time.perf_counter()
+    total_runtime = end_time - start_time
+    return total_runtime
