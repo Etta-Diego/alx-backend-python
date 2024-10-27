9b985e26ab67d04f28eb0c593b23e965b64d7865
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-27 19:22:28 +0100

    correct code format in 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -10,7 +10,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """sums a list of integer and floats"""
-    total = 0.0
-    for num in mxd_lst:
-        total += num
-    return total
+    return float(sum(mxd_lst))
