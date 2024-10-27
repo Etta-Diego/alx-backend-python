483cd5223a1e7e9261884de9af05a9334b6e7678
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-27 19:07:49 +0100

    add comment in 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -15,6 +15,7 @@ from typing import Protocol, Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """corrected duck-typed annotations"""
     if lst:
         return lst[0]
     else:
