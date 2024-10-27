2357793b974003f98a129935d5485b0c59574297
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-27 13:29:27 +0100

    add  5-sum_list.py ettadiego@DESKTOP-1C7KPLI:~/alx-backend

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-A type-annotated function define_variables that define
-the following variables with their specified values:
+Annotated and defined the following
+variables with their specified values:
 
 a, an integer with a value of 1
 pi, a float with a value of 3.14
@@ -11,16 +11,8 @@ school, a string with a value of “Holberton”
 """
 
 
-from typing import Tuple
-
-""" defining the variables """
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
-
-
-def define_variables(a: int, pi: float, i_understand_annotations:
-                     bool, school: str) -> Tuple[int, float, bool, str]:
-    """return the defined variables"""
-    return a, pi, i_understand_annotations, school
+""" defining the variables: a, pi, i_understand_annotations, school """
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+A type-annotated function sum_list which takes a list input_list
+of floats as argument and returns their sum as a float.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum up the list numbers"""
+    output_list = 0
+    for num in input_list:
+        output_list += num 
+    return output_list
