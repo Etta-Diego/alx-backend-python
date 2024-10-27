aa2c635ff309eafde1e8640d7bf20707361aad2b
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-27 18:57:53 +0100

    fulfil pycodestyle requirement in 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -17,7 +17,9 @@ from typing import Union, Any, Mapping, TypeVar
 """Initialize TypeVar to T"""
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """ Added type annotations to the function """
     if key in dct:
         return dct[key]
